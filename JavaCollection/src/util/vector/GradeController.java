abe728e6f1001942f8dff7466f1df859ab2a2803
SuperClass <SuperClass>
2015-07-28 15:19:33 +0900

    2015.07.28 컬렉션

--- JavaCollection/src/util/vector/GradeController.java
+++ JavaCollection/src/util/vector/GradeController.java
@@ -1,5 +1,7 @@
 package util.vector;
 
+import java.util.Scanner;
+
 public class GradeController {	
 	public static void main(String[] args) {
 		GradeService service = new GradeServiceImpl();
@@ -14,6 +16,77 @@ public class GradeController {
 		service.input(lee);
 		
 		//전체 학생 성적표를 출력
-		service.printList();
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		int input = 0, key = 0;
+		/*무한루프 엔진*/
+		while (true) {
+			/*메뉴판*/
+			System.out.println("1.성적입력 2.성적조회(학번) 3.성적조회(이름) 4.전체성적 5.성적순정렬 6.이름순정렬 0.종료");
+			input = scanner.nextInt();
+			/*입력값 체크*/
+			key = inputCheck(input, 0, 6);
+			if (input !=0) {
+				switch (key) {
+				case 1:	/*성적입력*/		
+					System.out.println("학번 : ");
+					String hakbun = scanner.next();
+					System.out.println("이름 : ");					
+					String name = scanner.next();
+					System.out.println("국어 : ");
+					int kor = scanner.nextInt();
+					System.out.println("영어 : ");
+					int eng = scanner.nextInt();
+					System.out.println("수학 : ");
+					int math = scanner.nextInt();
+					Grade grade = new Grade(hakbun, name, kor, eng, math);
+					service.input(grade);
+					break;
+				case 2:	/*성적조회(학번)*/	
+					System.out.println("학번입력 : ");
+					System.out.println(service.searchGradeByHakbun(scanner.next()));
+					break;
+				case 3:	/*성적조회(이름)*/	
+					System.out.println("이름을 입력하세요 : ");
+					System.out.println(service.searchGradeByName(scanner.next()));
+					break;
+				case 4:	/*전체성적*/	
+					service.printList();
+					break;
+				case 5:	/*성적순정력*/		
+					service.descByTotal();
+					break;
+				case 6:	/*이름순정렬*/		
+					service.ascByName();
+					break;
+				default: System.out.println("에러발생");
+					break;
+				}
+			} else {
+				System.out.println("시스텐 종료!!");
+				break;
+			}
+		}
+	}
+	private static int inputCheck(int input, int i, int j) {
+		if(input < i || input > j){
+			System.out.println("선택메뉴에서 범위값 외의 수를 입력했습니다.");
+			return 0; //시스템 종료
+		}
+		else{
+			return input; //input에대한 숫자 검증
+		}
+		
 	}
 }
+/*
+ 스캐너 장착
+ 
+ while(true){
+ 	if(input !=0 ){
+ 	
+ 	}else{
+ 	
+ 	}
+ * */
--- JavaCollection/src/util/vector/GradeServiceImpl.java
+++ JavaCollection/src/util/vector/GradeServiceImpl.java
@@ -1,11 +1,16 @@
 package util.vector;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Vector;
 
 public class GradeServiceImpl implements GradeService{
 	//필드에 아래 메소드들이 공유할 자료구조를 뭘 쓸까 고민
-	private Vector<Grade> vec = new Vector<Grade>();
+	private List<Grade> vec = new ArrayList();
 	Grade grade = new Grade();	//에러뜨면 디폴트 생성자를 만듬
+	
 	@Override
 	public void input(Grade grade) {
 		// 성적표 입력
@@ -26,13 +31,13 @@ public class GradeServiceImpl implements GradeService{
 		Grade grade = null;	//지변으로 인스턴스를 선언했으므로 초기화 해야함
 		
 		for (int i = 0; i < vec.size(); i++) {
-			String searchHakbun = vec.elementAt(i).getHakbun();
+			String searchHakbun = vec.get(i).getHakbun();
 			//배열로 생각하면  vec.get(i);
 			if (hakbun.equalsIgnoreCase(searchHakbun)) {
-				String name = vec.elementAt(i).getName();
-				int kor = vec.elementAt(i).getKor();
-				int eng = vec.elementAt(i).getEng();
-				int math = vec.elementAt(i).getMath();
+				String name = vec.get(i).getName();
+				int kor = vec.get(i).getKor();
+				int eng = vec.get(i).getEng();
+				int math = vec.get(i).getMath();
 				grade = new Grade(searchHakbun,name,kor,eng,math);
 				msg = grade.toString();
 				break;	//중간이라도 학번이 일치하면 그래도 stop
@@ -46,8 +51,79 @@ public class GradeServiceImpl implements GradeService{
 	@Override
 	public void ascGradeTotal() {
 		// 성적 정렬
-		
+	}
+	
+	@Override
+	public  Vector<Grade> searchGradeByName(String name) {
+		 Vector<Grade> temp = new  Vector<Grade>();
+		 String msg = "";
+		 Grade grade = null;
+		 
+		 for (int i = 0; i < vec.size(); i++) {
+			String searchName = vec.get(i).getName();
+			if (name.equalsIgnoreCase(searchName)) {
+				
+				grade = new Grade(vec.get(i).getHakbun(),
+						searchName,vec.get(i).getKor(),
+						vec.get(i).getEng(),vec.get(i).getMath());
+				temp.add(grade);
+				//msg = grade.toString();
+			} else {
+				temp.remove(new Grade());
+				//temp라는 백터를 완전히 비워서 null로 리턴
+			}
+		}
+		return temp;
 	}
 
+	@Override
+	public void descByTotal() {
+		//Java API중에서 정렬을 담당하는 클래스 Comparator
+		Comparator<Grade> desc = new Comparator<Grade>() {
+			
+			@Override
+			public int compare(Grade g1, Grade g2) {
+				//삼항연산자
+				/*
+				 if(condition){
+				 	--> true면 이구역(statement) 실행
+				 }else{
+				 	-> false면 이구역(statement) 실행
+				 } 
+				 (조건식) ? 참 : 거짓;
+				 **/
 
+//				if ((g1.getTotal()<g2.getTotal())) {
+//					return 1;					
+//				} else {
+//					if (g1.getTotal()==g2.getTotal()) {
+//						return 0;
+//					} else {
+//						return -1;
+//					}
+//				}
+				return (g1.getTotal()<g2.getTotal()) ? 1 : 
+					(g1.getTotal()==g2.getTotal()) ? 0 : -1;
+			}
+		};
+		Collections.sort(vec, desc);
+		System.out.println(vec.toString());
+	}
+
+	@Override
+	public void ascByName() {
+		Comparator<Grade> asc = new Comparator<Grade>() {
+			
+			@Override
+			public int compare(Grade g1, Grade g2) {
+				// 값()이 int타입이 아니고
+				// String 타입의 우선순위 결정할 때는
+				// compareTo 메소드를 사용해야 한다.
+				//return (g1.getTotal() > g2.getTotal()) ? 1 : (g1.getTotal() == g2.getTotal()) ? 0 : -1;
+				return g1.getName().compareTo(g2.getName());
+			}
+		};
+		Collections.sort(vec, asc);
+		System.out.println(vec.toString());
+	}
 }
